5f521ed9cf6e09ebf0c0ee9ca9d8460c34241a9e
signalkee <kee.inho0@gmail.com>
2024-11-13 16:17:39 -0500

    Modified to simplified model

--- robots/kinematic_bicycle2D.py
+++ robots/kinematic_bicycle2D.py
@@ -2,7 +2,8 @@ import numpy as np
 import casadi as ca
 
 """
-Kinematic Bicycle Model for CBF-QP and MPC-CBF (casadi)
+Simplified kinematic bicycle model for CBF-QP and MPC-CBF (casadi)
+https://arxiv.org/abs/2403.07043
 """
 
 def angle_normalize(x):
@@ -19,18 +20,18 @@ def angle_normalize(x):
 class KinematicBicycle2D:
     def __init__(self, dt, robot_spec):
         '''
-            X: [x, y, theta, delta]
-            U: [v, delta_dot] (velocity, steering rate)
-            cbf: h(x) = ||x-x_obs||^2 - beta*d_min^2
-            relative degree: 1
+            X: [x, y, theta, v]
+            U: [a, β] (acceleration, slip angle as control input)
+            cbf: h(x) = ||x-x_obs||^2 - β*d_min^2
+            relative degree: 2
             
             Equations:
-            - β = arctan((L_r / L) * tan(δ)) (Slip angle)
+            β = arctan((L_r / L) * tan(δ)) (Slip angle)
             
-            - x_dot = v * cos(θ + β)
-            - y_dot = v * sin(θ + β)
-            - θ_dot = v / L * cos(β) * tan(δ)
-            - δ_dot = δ_dot (input)
+            x_dot = v * cos(theta) - v * sin(theta) * β
+            y_dot = v * sin(theta) + v * cos(theta) * β
+            theta_dot = (v / L_r) * β
+            v_dot = a
         '''
         self.dt = dt
         self.robot_spec = robot_spec
@@ -51,42 +52,67 @@ class KinematicBicycle2D:
         L = self.robot_spec['wheel_base']
         return np.arctan((L_r / L) * np.tan(delta))
             
+    def beta_to_delta(self, beta):
+        # Map slip angle beta to steering angle delta
+        L_r = self.robot_spec['rear_axle_distance']
+        L = self.robot_spec['wheel_base']
+        return np.arctan((L / L_r) * np.tan(beta))
+            
+    def df_dx(self, X):
+        return np.array([
+            [0, 0, -X[3, 0]*np.sin(X[2, 0]), np.cos(X[2, 0])],
+            [0, 0,  X[3, 0]*np.cos(X[2, 0]), np.sin(X[2, 0])],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ])
+
     def f(self, X, casadi=False):
+        theta = X[2, 0]
+        v = X[3, 0]
         if casadi:
             return ca.vertcat(
-                0,
-                0,
-                0,
-                0
+                v * ca.cos(theta), 
+                v * ca.sin(theta), 
+                0,                 
+                0                  
             )
         else:
-            return np.array([0, 0, 0, 0]).reshape(-1, 1)
+            return np.array([
+                v * np.cos(theta), 
+                v * np.sin(theta), 
+                0,                 
+                0                  
+            ]).reshape(-1, 1)
 
     def g(self, X, casadi=False):
-        delta = X[3, 0]
-        beta = self.beta(delta)
-
+        theta = X[2, 0]
+        v = X[3, 0]
+        L_r = self.robot_spec['rear_axle_distance']
         if casadi:
             g = ca.SX.zeros(4, 2)
-            g[0, 0] = ca.cos(X[2, 0] + beta)
-            g[1, 0] = ca.sin(X[2, 0] + beta)
-            g[2, 0] = ca.cos(beta) * ca.tan(delta) / self.L
-            g[3, 1] = 1
+            g[0, 0] = -v * ca.sin(theta) 
+            g[1, 0] = v * ca.cos(theta)  
+            g[2, 1] = v / L_r       
+            g[3, 0] = 1                  
             return g
         else:
             return np.array([
-                [np.cos(X[2, 0] + beta), 0],
-                [np.sin(X[2, 0] + beta), 0],
-                [np.cos(beta) * np.tan(delta) / self.L, 0],
-                [0, 1]
+                [0, -v * np.sin(theta)],
+                [0, v * np.cos(theta)],
+                [0, v / L_r],
+                [1, 0]
             ])
 
     def step(self, X, U):
-        X = X + (self.f(X) + self.g(X) @ U) * self.dt
-        X[2, 0] = angle_normalize(X[2, 0]) 
+        a, beta = U.flatten() 
+        delta = self.beta_to_delta(beta)  # Convert beta to delta
+        U_with_delta = np.array([a, delta]).reshape(-1, 1)  # Replace beta with delta
+
+        X = X + (self.f(X) + self.g(X) @ U_with_delta) * self.dt
+        X[2, 0] = angle_normalize(X[2, 0])
         return X
    
-    def nominal_input(self, X, G, d_min=0.05, k_omega=2.0, k_v=1.0):
+    def nominal_input(self, X, G, d_min=0.05, k_theta=2.0, k_v=1.0):
         '''
         nominal input for CBF-QP
         '''
@@ -96,50 +122,64 @@ class KinematicBicycle2D:
         theta_d = np.arctan2(G[1, 0] - X[1, 0], G[0, 0] - X[0, 0])
         error_theta = angle_normalize(theta_d - X[2, 0])
 
-        delta_dot = k_omega * error_theta  # Steering velocity
+        beta = k_theta * error_theta  # Steering velocity in terms of slip angle
+        delta = self.beta_to_delta(beta)
+        
         if abs(error_theta) > np.deg2rad(90):
-            v = 0.0
+            a = 0.0
         else:
-            v = k_v * distance * np.cos(error_theta)
+            a = k_v * distance * np.cos(error_theta)
             
-        return np.array([v, delta_dot]).reshape(-1, 1)
+        return np.array([a, delta]).reshape(-1, 1)
     
     def stop(self, X):
         return np.array([0, 0]).reshape(-1, 1)
 
     def has_stopped(self, X, tol=0.05):
-        # Kinematic bicycle model can always stop immediately
-        return True
+        return abs(X[3, 0]) < tol  
 
-    def rotate_to(self, X, theta_des, k_omega=2.0):
+    def rotate_to(self, X, theta_des, k_theta=2.0):
         error_theta = angle_normalize(theta_des - X[2, 0])
-        delta_dot = k_omega * error_theta
-        return np.array([0.0, delta_dot]).reshape(-1, 1)
+        beta = k_theta * error_theta
+        delta = self.beta_to_delta(beta)
+        return np.array([0.0, delta]).reshape(-1, 1)
 
     def agent_barrier(self, X, obs, robot_radius, beta=1.01):
         '''Continuous Time High Order CBF'''
         obsX = obs[0:2]
-        d_min = obs[2][0] + robot_radius # obs radius + robot radius
+        d_min = obs[2][0] + robot_radius  # obs radius + robot radius
 
-        h = np.linalg.norm(X[0:2] - obsX[0:2])**2 - beta * d_min**2
-        dh_dx = 2 * (X[0:2] - obsX[0:2]).T
-        return h, dh_dx
+        h = np.linalg.norm(X[0:2] - obsX[0:2])**2 - beta*d_min**2
+        # Lgh is zero => relative degree is 2
+        h_dot = 2 * (X[0:2] - obsX[0:2]).T @ (self.f(X)[0:2])
+
+        df_dx = self.df_dx(X)
+        dh_dot_dx = np.append((2 * self.f(X)[0:2]).T, np.array(
+            [[0, 0]]), axis=1) + 2 * (X[0:2] - obsX[0:2]).T @ df_dx[0:2, :]
+        return h, h_dot, dh_dot_dx
 
     def agent_barrier_dt(self, x_k, u_k, obs, robot_radius, beta=1.01):
         '''Discrete Time High Order CBF'''
+        # Dynamics equations for the next states
         x_k1 = self.step(x_k, u_k)
+        x_k2 = self.step(x_k1, u_k)
 
         def h(x, obs, robot_radius, beta=1.01):
+            '''Computes CBF h(x) = ||x-x_obs||^2 - beta*d_min^2'''
             x_obs = obs[0]
             y_obs = obs[1]
             r_obs = obs[2]
             d_min = robot_radius + r_obs
-            
-            h = (x[0, 0] - x_obs)**2 + (x[1, 0] - y_obs)**2 - beta * d_min**2
+
+            h = (x[0, 0] - x_obs)**2 + (x[1, 0] - y_obs)**2 - beta*d_min**2
             return h
 
+        h_k2 = h(x_k2, obs, robot_radius, beta)
         h_k1 = h(x_k1, obs, robot_radius, beta)
         h_k = h(x_k, obs, robot_radius, beta)
 
         d_h = h_k1 - h_k
-        return h_k, d_h
+        dd_h = h_k2 - 2 * h_k1 + h_k
+        # hocbf_2nd_order = h_ddot + (gamma1 + gamma2) * h_dot + (gamma1 * gamma2) * h_k
+
+        return h_k, d_h, dd_h
