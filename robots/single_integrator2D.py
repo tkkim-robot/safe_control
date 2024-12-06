ce9fa97077e5f6caddf4834a2b3e7367462aadf8
parkcart1595 <parkcart1595@gmail.com>
2024-12-06 00:12:54 -0500

    2nd edit single_int

--- robots/single_integrator2D.py
+++ robots/single_integrator2D.py
@@ -3,10 +3,12 @@ import casadi as ca # 최적화 및 자동 미분을 위한 라이브러리
 
 """
 Created on July 15th, 2024
+Edited on Dec 5th, 2024
 @author: Taekyung Kim
+@editor: Hun Kuk Park
 
 @description: 
-Double Integrator model for CBF-QP and MPC-CBF (casadi) with separated position and attitude states
+Single Integrator model for CBF-QP and MPC-CBF (casadi) with separated position and attitude states
 """
 
 
@@ -25,12 +27,10 @@ class SingleIntegrator2D:
 
     def __init__(self, dt, robot_spec):
         '''
-            X: [x, y, vx, vy]
-            theta: yaw angle
-            U: [ax, ay]
-            U_attitude: [yaw_rate]
+            X: [x, y]
+            U: [vx, vy]
             cbf: h(x) = ||x-x_obs||^2 - beta*d_min^2
-            relative degree: 2
+            relative degree: 1
         '''
         self.dt = dt # instance 변수에 저장
         self.robot_spec = robot_spec
@@ -39,18 +39,16 @@ class SingleIntegrator2D:
         #if 'a_max' not in self.robot_spec:
         #    self.robot_spec['a_max'] = 1.0
 
+    # f(x)
     def f(self, X, casadi=False): # casadi 기본값으로 사용 x
         if casadi:
             return ca.vertcat( # casadi에서 벡터를 수직으로 연결하는 함수
-                X[2, 0],
-                X[3, 0],
                 0,
                 0
             )
         else:
-            return np.array([X[2, 0], # numpy배열을 생성
-                             X[3, 0],
-                             0,
+            return np.array([
+                             0, # numpy배열을 생성
                              0]).reshape(-1, 1)
 
     def df_dx(self, X):
@@ -64,21 +62,19 @@ class SingleIntegrator2D:
     def g(self, X, casadi=False):
         if casadi:
             return ca.DM([
-                [0, 0],
-                [0, 0],
                 [1, 0],
                 [0, 1]
             ])
         else:
-            return np.array([[0, 0], [0, 0], [1, 0], [0, 1]])
+            return np.array([[1, 0], [0, 1]])
 
     def step(self, X, U):
         X = X + (self.f(X) + self.g(X) @ U) * self.dt
         return X
 
-    def step_rotate(self, theta, U_attitude):
-        theta = angle_normalize(theta + U_attitude[0, 0] * self.dt)
-        return theta
+    # def step_rotate(self, theta, U_attitude):
+    #     theta = angle_normalize(theta + U_attitude[0, 0] * self.dt)
+    #     return theta
 
     def nominal_input(self, X, G, d_min=0.05, k_v=1.0, k_a=1.0): # 위치제어를 위한 명령 입력(가속도) 생성
         '''
@@ -105,21 +101,21 @@ class SingleIntegrator2D:
         # if a_mag > a_max:
         #     a = a * a_max / a_mag
 
-        return a.reshape(-1, 1) # 계산된 가속도를 열 벡터로 변환
+        return v_des.reshape(-1, 1) # 계산된 가속도를 열 벡터로 변환
 
-    def nominal_attitude_input(self, theta, theta_des, k_theta=1.0): # 자세 제어 입력 생성 함수: 원하는 각도로 회전하기 위한 명령 입력
-        '''
-        nominal input for attitude control
-        '''
-        error_theta = angle_normalize(theta_des - theta)
-        yaw_rate = k_theta * error_theta
-        return np.array([yaw_rate]).reshape(-1, 1)
+    # def nominal_attitude_input(self, theta, theta_des, k_theta=1.0): # 자세 제어 입력 생성 함수: 원하는 각도로 회전하기 위한 명령 입력
+    #     '''
+    #     nominal input for attitude control
+    #     '''
+    #     error_theta = angle_normalize(theta_des - theta)
+    #     yaw_rate = k_theta * error_theta
+    #     return np.array([yaw_rate]).reshape(-1, 1)
 
     def stop(self, X, k_a=1.0): # 정지명령 생성 함수
         # Set desired velocity to zero
         vx_des, vy_des = 0.0, 0.0
-        ax = k_a * (vx_des - X[2, 0])
-        ay = k_a * (vy_des - X[3, 0])
+        # ax = k_a * (vx_des - X[2, 0])
+        # ay = k_a * (vy_des - X[3, 0])
         return np.array([ax, ay]).reshape(-1, 1)
 
     def has_stopped(self, X, tol=0.05):
@@ -136,14 +132,14 @@ class SingleIntegrator2D:
         d_min = obs[2][0] + robot_radius  # obs radius + robot radius
 
         h = np.linalg.norm(X[0:2] - obsX[0:2])**2 - beta*d_min**2 # 로봇과 장애물 사이의 거리 제곱에서 안전 거리 제곱을 뺀 값
-        # Lgh is zero => relative degree is 2, f(x)[0:2] actually equals to X[2:4]
+        # relative degree is 1, f(x)[0:2] actually equals to X[2:4]
         h_dot = 2 * (X[0:2] - obsX[0:2]).T @ (self.f(X)[0:2]) # h의 미분
 
         # these two options are the same
         # df_dx = self.df_dx(X)
         # dh_dot_dx = np.append( ( 2 * self.f(X)[0:2] ).T, np.array([[0,0]]), axis = 1 ) + 2 * ( X[0:2] - obsX[0:2] ).T @ df_dx[0:2,:]
-        dh_dot_dx = np.append(2 * X[2:4].T, 2 * (X[0:2] - obsX[0:2]).T, axis=1) # h_dot을 x에 대해 편미분한 값
-        return h, h_dot, dh_dot_dx
+        # dh_dot_dx = np.append(2 * X[2:4].T, 2 * (X[0:2] - obsX[0:2]).T, axis=1) # h_dot을 x에 대해 편미분한 값
+        return h, h_dot
 
     def agent_barrier_dt(self, x_k, u_k, obs, robot_radius, beta=1.01):
         '''Discrete Time High Order CBF'''
@@ -166,7 +162,7 @@ class SingleIntegrator2D:
         h_k = h(x_k, obs, robot_radius, beta)
 
         d_h = h_k1 - h_k
-        dd_h = h_k2 - 2 * h_k1 + h_k
+        # dd_h = h_k2 - 2 * h_k1 + h_k
         # hocbf_2nd_order = h_ddot + (gamma1 + gamma2) * h_dot + (gamma1 * gamma2) * h_k
 
-        return h_k, d_h, dd_h
+        return h_k, d_h
